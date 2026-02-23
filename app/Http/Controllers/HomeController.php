73542fed3ed225f54d17403dc78bedfd3c2e5eb2
AncodeTower <carlosandres4020@gmail.com>
2026-02-23 01:43:26 -0600

    Remove default Blade welcome view to declutter the application structure

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PhpParser\Node\Scalar\String_;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $title = 'Dashboard';
+        return view('home', compact('title'));
+    }
+}
