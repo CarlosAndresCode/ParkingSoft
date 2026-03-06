c160acd1f1c315b26cfedfecd39060acf320a8d7
AncodeTower <carlosandres4020@gmail.com>
2026-03-05 23:22:09 -0500

    Add demo user creation in DatabaseSeeder for local environment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,16 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RateSeeder::class
         ]);
+
+        if(config('app.env') === 'local') { // Demo user for testing
+            User::create([
+                'name' => 'Cashier User',
+                'email' => 'cashier@cahier.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'role_id' => 2, // Cashier
+            ]);
+        }
     }
 }
